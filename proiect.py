e2651d300ec9e3937fce1d815add7e8e01c40c63
Andrei Halauca <ahalauca@gmail.com>
2020-05-23 13:23:09 +0300

    refactor

--- proiect.py
+++ proiect.py
@@ -1,17 +1,7 @@
-# De instalat:
-#       Gephi
-#       Outwit
-#       Pajek
+import json
+import requests
 
-# Plugin excel: Nodexl
-
-# Ted Ideas Worth Spreading
-
-# TWITCH API ramane, trebuie sa caut
-import requests, json, twitch
-
-
-#Pentru luat OAUTH token
+# Pentru luat OAUTH token
 # https://id.twitch.tv/oauth2/authorize
 #     ?client_id=n23ybd5bk9rr48r0cr669laek7b4pj
 #     &redirect_uri=http://localhost
@@ -23,10 +13,12 @@ CLIENT_ID = 'n23ybd5bk9rr48r0cr669laek7b4pj'
 OAUTH_TOKEN = '0f0c2fz1ighm085hc97dqagbb53h64'
 HEADERS = {
     'Client-ID': CLIENT_ID,
-    'Authorization': 'Bearer '+OAUTH_TOKEN
+    'Authorization': 'Bearer ' + OAUTH_TOKEN
 }
 BASE_URL = 'https://api.twitch.tv/helix/'
 TOP_GAMES_URL = 'https://api.twitch.tv/helix/games/top'
+TOP_STREAMS_URL = 'https://api.twitch.tv/helix/streams'
+GAMES_URL = 'https://api.twitch.tv/helix/games'
 
 
 def get_response(input_url, query):
@@ -35,26 +27,44 @@ def get_response(input_url, query):
     return response
 
 
-def manage_response(response):
+def print_response(response):
     response_json = response.json()
     printed_response = json.dumps(response_json, indent=4)
     print(printed_response)
-    with open("top_games.json", "w", encoding='utf-8') as fd:
-        json.dump(response_json, fd, indent=4)
 
 
-def get_user_statistics(username):
-    dict = {}
-    dict['Username'] = helix.user(username).display_name
-    dict['Is Live'] = helix.user(username).is_live
-    dict['Stream'] = helix.user(username).stream
-    print(dict)
+def export_response(response):
+    response = response.json()
+    with open("top_stream.json", "w", encoding='utf-8') as fd:
+        json.dump(response, fd, indent=4)
+
+
+def get_top_20_streams():
+    response = get_response(TOP_STREAMS_URL, '')
+    print_response(response)
+    return response
+
+
+def get_top_20_activities():
+    response = get_response(TOP_GAMES_URL, '')
+    print_response(response)
+    return response
+
+
+# TODO:Top 20 pe un anumit joc/activitate
+def get_top_20_streams_by_activity(activity):
+    pass
+
+
+# TODO:ceva despre utilizatori, dar nu stiu ce
 
 
 if __name__ == '__main__':
-    helix = twitch.Helix(client_id=CLIENT_ID, bearer_token=OAUTH_TOKEN)
-    get_user_statistics('sneakylol')
+    # helix = twitch.Helix(client_id=CLIENT_ID, bearer_token=OAUTH_TOKEN)
 
-    # streams_query = 'streams?game_id=33214'
-    # response = get_response(TOP_GAMES_URL,'')
-    # manage_response(response)
+    # top_20_streams = get_top_20_streams()
+    # export_response(top_20_streams)
+
+
+    top_20_activities = get_top_20_activities()
+    # export_response(top_20_activities)
