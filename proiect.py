c1fd5729073795675efb7dbb1599b3a78286a651
catalin99 <catalinbelu21@gmail.com>
2020-05-28 11:41:39 +0300

    multe jsoane

--- proiect.py
+++ proiect.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from datetime import date, datetime
+import json as JSON
 import pandas as pd
 
 # Pentru luat OAUTH token
@@ -21,6 +22,8 @@ BASE_URL = 'https://api.twitch.tv/helix/'
 TOP_GAMES_URL = 'https://api.twitch.tv/helix/games/top'
 TOP_STREAMS_URL = 'https://api.twitch.tv/helix/streams'
 GAMES_URL = 'https://api.twitch.tv/helix/games'
+USERS_FOLLOWERS_URL = "https://api.twitch.tv/helix/users/follows"
+USERS_FOLLOWED_URL = "https://api.twitch.tv/helix/users/follows"
 
 
 def get_response(input_url, query):
@@ -60,8 +63,23 @@ def get_top_20_activities():
 def get_top_20_streams_by_activity(id_activity, name_activity):
     response = get_response(TOP_STREAMS_URL, '?game_id=' + id_activity)
     export_response(response, "top20_streams_" + name_activity)
-    print_response(response)
+    # print_response(response)
 
+def get_users_ids_from_json(json):
+    topIDs = []
+    with open(json) as data_file:
+        data = JSON.load(data_file)
+        for v in data["data"]:
+            topIDs.append((v["user_id"],v["user_name"]))
+    return topIDs
+
+def get_user_followers(user):
+    response = get_response(USERS_FOLLOWERS_URL, '?to_id='+user[0])
+    export_response(response, "top20_followers_" + user[1])
+
+def get_user_followed_by(user):
+    response = get_response(USERS_FOLLOWED_URL, '?from_id='+user[0])
+    export_response(response, "top20_followed_by_" + user[1])
 
 # TODO:ceva despre utilizatori, dar nu stiu ce
 
@@ -76,8 +94,13 @@ if __name__ == '__main__':
 
     # df = pd.read_json(r'W:\Faculty\ARMS\top20_activities-20200523_1421.json')
     # export_csv = df.to_csv(r'W:\Faculty\ARMS\top20_activities-20200523_1421.csv', index=None, header=True)
-    df = pd.read_json(r'W:\Faculty\ARMS\top20_streams-20200523_1421.json')
-    export_csv = df.to_csv(r'W:\Faculty\ARMS\top20_streams-20200523_1421.csv', index=None, header=True)
+    # df = pd.read_json(r'W:\Faculty\ARMS\top20_streams-20200523_1421.json')
+    # export_csv = df.to_csv(r'W:\Faculty\ARMS\top20_streams-20200523_1421.csv', index=None, header=True)
     # VALORANT - 516575
     # Grand Theft Auto V - 32982
     # Dota 2 - 29595
+
+    users = get_users_ids_from_json('top20_streams-20200527_1043.json')
+    for user in users:
+        get_user_followers(user)
+        get_user_followed_by(user)
